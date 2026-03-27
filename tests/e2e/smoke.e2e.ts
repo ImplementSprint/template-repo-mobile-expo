04bc662aef85c762a564a9c0ea3a84b2c48f5eda
tone <antoneeeeetorres@gmail.com>
2026-03-27 17:14:01 +0800

    test(e2e): add app launch before smoke test execution

--- tests/e2e/smoke.e2e.ts
+++ tests/e2e/smoke.e2e.ts
@@ -1,6 +1,10 @@
-import { by, element, expect } from 'detox';
+import { by, device, element, expect } from 'detox';
 
 describe('Boilerplate app smoke flow', () => {
+  beforeAll(async () => {
+    await device.launchApp({ newInstance: true });
+  });
+
   it('placeholder e2e test', async () => {
     await expect(element(by.text('Expo TS Boilerplate'))).toBeVisible();
   });
