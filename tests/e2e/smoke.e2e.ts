5cc3e0a4a8052f60c2025ff461991af8d82157cd
tone <antoneeeeetorres@gmail.com>
2026-03-27 16:46:44 +0800

    test(e2e): update smoke test to check visibility of the main app text

--- tests/e2e/smoke.e2e.ts
+++ tests/e2e/smoke.e2e.ts
@@ -1,5 +1,7 @@
+import { by, element, expect } from 'detox';
+
 describe('Boilerplate app smoke flow', () => {
   it('placeholder e2e test', async () => {
-    expect(true).toBe(true);
+    await expect(element(by.text('Expo TS Boilerplate'))).toBeVisible();
   });
 });
