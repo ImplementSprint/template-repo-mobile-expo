d2b621bbfe8be37cf165191fec2a76b844865b20
tone <antoneeeeetorres@gmail.com>
2026-04-04 03:02:12 +0800

    test(unit): fix App test import paths for lint/jest

--- tests/unit/App.test.tsx
+++ tests/unit/App.test.tsx
@@ -1,7 +1,7 @@
 import { Children, isValidElement } from 'react';
 
-import App from '../../../src/bootstrap/App';
-import { HomeScreen } from '../../../src/features/home/screens/HomeScreen';
+import App from '../../src/bootstrap/App';
+import { HomeScreen } from '../../src/features/home/screens/HomeScreen';
 
 describe('App', () => {
   it('renders a status bar and home screen', () => {
